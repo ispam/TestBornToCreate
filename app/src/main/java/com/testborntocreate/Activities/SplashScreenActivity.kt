a93b4057ae26620ae5355872df0707a97bca0976
Ispam <diegoug@gmail.com>
2019-02-07 13:36:07 -0500

    Added splash screen delay and finished with the app

--- app/src/main/java/com/testborntocreate/Activities/SplashScreenActivity.kt
+++ app/src/main/java/com/testborntocreate/Activities/SplashScreenActivity.kt
@@ -1,13 +1,38 @@
 package com.testborntocreate.Activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.testborntocreate.R
+import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private val disposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        disposable.add(Completable.complete()
+            .subscribeOn(Schedulers.newThread())
+            .delay(2500, TimeUnit.MILLISECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnComplete {
+                val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                startActivity(intent)
+                finish()
+            }
+            .subscribe())
+    }
+
+    override fun onStop() {
+        if (!disposable.isDisposed) disposable.clear()
+        super.onStop()
     }
 }
